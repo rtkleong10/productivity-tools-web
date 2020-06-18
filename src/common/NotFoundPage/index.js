da475d40eee540e7dea83bece70f81f482265fbd
rtkleong10 <rtkleong10@gmail.com>
2020-06-18 19:46:16 +0800

    [update] Updated the design of the not found page

--- src/common/NotFoundPage/index.js
+++ src/common/NotFoundPage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { Helmet } from "react-helmet";
 
+import notFound from './not-found.svg';
 import Button from '../../components/Button';
 
 export default function NotFoundPage() {
@@ -10,9 +11,14 @@ export default function NotFoundPage() {
 			<Helmet>
 				<title>Not Found</title>
 			</Helmet>
-			<h1>404 Not Found</h1>
-			<p>Sorry, we can't seem to find the page that you're looking for.</p>
-			<Button isLink={true} to="/" icon={faChevronLeft}>Return to Home</Button>
+			<div className="center mt-40">
+				<img src={notFound} />
+				<div className="mb-20">
+					<h1>404 Not Found</h1>
+					<p>Sorry, we can't seem to find the page that you're looking for.</p>
+				</div>
+				<Button isLink={true} to="/" icon={faChevronLeft}>Return to Home</Button>
+			</div>
 		</div>
 	)
 }
