34910dbd65afb0b25ef2f610c3f9e722bbe048ce
rtkleong10 <rtkleong10@gmail.com>
2020-06-19 16:54:24 +0800

    Updates

---
+++ precache-manifest.770d43a798aad8a6f8800490d93a2096.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9f1077f6c6663940995c4423a92475",
+    "url": "/productivity-tools-web/index.html"
+  },
+  {
+    "revision": "f11f3b25b96760e71d3a",
+    "url": "/productivity-tools-web/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "40876ce838726d6aa9b7",
+    "url": "/productivity-tools-web/static/css/main.1f78fc2d.chunk.css"
+  },
+  {
+    "revision": "f11f3b25b96760e71d3a",
+    "url": "/productivity-tools-web/static/js/2.b40d379d.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/productivity-tools-web/static/js/2.b40d379d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40876ce838726d6aa9b7",
+    "url": "/productivity-tools-web/static/js/main.776ef7c7.chunk.js"
+  },
+  {
+    "revision": "e62bc5a23bf12159340c",
+    "url": "/productivity-tools-web/static/js/runtime-main.d8d21f52.js"
+  },
+  {
+    "revision": "61887e1e950488f7a52971725b2ebda6",
+    "url": "/productivity-tools-web/static/media/AvenirNext-Bold-01.61887e1e.ttf"
+  },
+  {
+    "revision": "2538a3f00a198337bb2911bd6f3182ae",
+    "url": "/productivity-tools-web/static/media/AvenirNext-DemiBold-03.2538a3f0.ttf"
+  },
+  {
+    "revision": "597381f75a1b983328f95e3966e929f6",
+    "url": "/productivity-tools-web/static/media/AvenirNext-Medium-06.597381f7.ttf"
+  },
+  {
+    "revision": "4d8fdeb265ff6d34fb3bd8e4292665c0",
+    "url": "/productivity-tools-web/static/media/AvenirNext-Regular-08.4d8fdeb2.ttf"
+  },
+  {
+    "revision": "6619ed79eddcb52db60f22c330f95f1f",
+    "url": "/productivity-tools-web/static/media/Futura-Bold-03.6619ed79.ttf"
+  },
+  {
+    "revision": "f90a2ad8bed9ee750a6297626d3b31f6",
+    "url": "/productivity-tools-web/static/media/Futura-Medium-01.f90a2ad8.ttf"
+  },
+  {
+    "revision": "afc6d93135310b6cb3ff5c830e850b48",
+    "url": "/productivity-tools-web/static/media/empty.afc6d931.svg"
+  },
+  {
+    "revision": "ca39146a14f0a51e81897b338c6e8564",
+    "url": "/productivity-tools-web/static/media/empty.ca39146a.svg"
+  },
+  {
+    "revision": "4424515ec7cdad6d021aa72eee952801",
+    "url": "/productivity-tools-web/static/media/futura-bold-03-webfont.4424515e.woff2"
+  },
+  {
+    "revision": "8c3574a38a5ae6720bbbe9b916a94a3c",
+    "url": "/productivity-tools-web/static/media/futura-bold-03-webfont.8c3574a3.woff"
+  },
+  {
+    "revision": "6bc13667cd4885c72a9998af9359bbc9",
+    "url": "/productivity-tools-web/static/media/futura-medium-01-webfont.6bc13667.woff2"
+  },
+  {
+    "revision": "87da75bf7d6840ac7d33167f62235d3e",
+    "url": "/productivity-tools-web/static/media/futura-medium-01-webfont.87da75bf.woff"
+  },
+  {
+    "revision": "b882863e42791f7d124ade95f298ad50",
+    "url": "/productivity-tools-web/static/media/not-found.b882863e.svg"
+  }
+]);
