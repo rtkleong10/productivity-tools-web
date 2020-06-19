6f7dab982fc575cbe5f6968b2032e656c2f33205
rtkleong10 <rtkleong10@gmail.com>
2020-06-19 16:49:28 +0800

    [fix] Fixed issues for deployment - Updated root url - Removed use of redux chrome extension

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
-export const AUTH_URL = 'http://127.0.0.1:8000/auth';
-export const API_URL = 'http://127.0.0.1:8000/api';
+export const ROOT_URL = 'https://rtkleong10-productivity-tools.herokuapp.com';
+// export const ROOT_URL = 'http://127.0.0.1:8000';
+
+export const AUTH_URL = `${ROOT_URL}/auth`;
+export const API_URL = `${ROOT_URL}/api`;
 
 export const MODAL_TYPES = {
 	CREATE: 'createModalVisible',
