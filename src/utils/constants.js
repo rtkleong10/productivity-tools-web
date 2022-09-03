35fd86d0b3097fc4d2a6859abd6518b0cf1b1c26
rtkleong10 <rtkleong10@gmail.com>
2022-09-04 00:17:36 +0800

    Change root url from heroku to fly.io

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const ROOT_URL = 'https://rtkleong10-productivity-tools.herokuapp.com';
+export const ROOT_URL = 'https://rtkleong10-productivity-tools.fly.dev';
 // export const ROOT_URL = 'http://127.0.0.1:8000';
 
 export const AUTH_URL = `${ROOT_URL}/auth`;
